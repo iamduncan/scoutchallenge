d9a2357a7081e4362b2d1d38d60a8148f62ac83b
Duncan Brown <iamduncan@users.noreply.github.com>
2023-04-02 13:30:18 +0100

    :white_check_mark: test(app): vitest config updated

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,14 @@
 /// <reference types="vitest" />
-/// <reference types="vite/client" />
 
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    // react(), 
+    // tsconfigPaths()
+  ],
   test: {
     globals: true,
     environment: "happy-dom",
